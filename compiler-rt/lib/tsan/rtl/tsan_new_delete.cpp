0a43028092622e22776046c5a3f9e44e6760d49c
Zyyy <e1351057@u.nus.edu>
2025-03-09 16:36:11 +0800

    [EXPER] remove calls to internal function
    * to measure the overhead of the interceptors

--- compiler-rt/lib/tsan/rtl/tsan_new_delete.cpp
+++ compiler-rt/lib/tsan/rtl/tsan_new_delete.cpp
@@ -25,37 +25,20 @@ enum class align_val_t: __sanitizer::uptr {};
 }  // namespace std
 
 DECLARE_REAL(void *, malloc, uptr size)
+DECLARE_REAL(void *, memalign, uptr align, uptr size)
 DECLARE_REAL(void, free, void *ptr)
 
 // TODO(alekseys): throw std::bad_alloc instead of dying on OOM.
 #define OPERATOR_NEW_BODY(mangled_name, nothrow) \
-  if (in_symbolizer()) \
-    return InternalAlloc(size); \
-  void *p = 0; \
-  {  \
-    SCOPED_INTERCEPTOR_RAW(mangled_name, size); \
-    p = user_alloc(thr, pc, size); \
-    if (!nothrow && UNLIKELY(!p)) { \
-      GET_STACK_TRACE_FATAL(thr, pc); \
-      ReportOutOfMemory(size, &stack); \
-    } \
-  }  \
-  invoke_malloc_hook(p, size);  \
+  if (in_symbolizer())                           \
+    return InternalAlloc(size);                  \
+  void *p = REAL(malloc)(size);                  \
   return p;
 
 #define OPERATOR_NEW_BODY_ALIGN(mangled_name, nothrow) \
-  if (in_symbolizer()) \
-    return InternalAlloc(size, nullptr, (uptr)align); \
-  void *p = 0; \
-  {  \
-    SCOPED_INTERCEPTOR_RAW(mangled_name, size); \
-    p = user_memalign(thr, pc, (uptr)align, size); \
-    if (!nothrow && UNLIKELY(!p)) { \
-      GET_STACK_TRACE_FATAL(thr, pc); \
-      ReportOutOfMemory(size, &stack); \
-    } \
-  }  \
-  invoke_malloc_hook(p, size);  \
+  if (in_symbolizer())                                 \
+    return InternalAlloc(size, nullptr, (uptr)align);  \
+  void *p = REAL(memalign)((uptr)align, size);         \
   return p;
 
 SANITIZER_INTERFACE_ATTRIBUTE
@@ -113,12 +96,11 @@ void *operator new[](__sanitizer::uptr size, std::align_val_t align,
 }
 
 #define OPERATOR_DELETE_BODY(mangled_name) \
-  if (ptr == 0) return;  \
-  if (in_symbolizer()) \
-    return InternalFree(ptr); \
-  invoke_free_hook(ptr);  \
-  SCOPED_INTERCEPTOR_RAW(mangled_name, ptr);  \
-  user_free(thr, pc, ptr);
+  if (ptr == 0)                            \
+    return;                                \
+  if (in_symbolizer())                     \
+    return InternalFree(ptr);              \
+  REAL(free)(ptr)
 
 SANITIZER_INTERFACE_ATTRIBUTE
 void operator delete(void *ptr) NOEXCEPT;
